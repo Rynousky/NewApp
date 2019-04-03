750ae41a45192c028a1f9e404ce1a6ba72334131
student <student@hubu.edu.cn>
2019-04-03 10:23:16 +0800

    1

---
+++ src/pages/index/index.vue
@@ -0,0 +1,174 @@
+<template>
+  <div @click="clickHandle">
+    <i-notice-bar icon="systemprompt" loop>
+    2019年4月1日举行吃货节
+    </i-notice-bar>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/1.png" />
+        </i-grid-icon>
+        <i-grid-label>西点</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/2.png" />
+        </i-grid-icon>
+        <i-grid-label>主食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/3.png" />
+        </i-grid-icon>
+        <i-grid-label>外卖</i-grid-label>
+    </i-grid-item>
+</i-grid>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/4.png" />
+        </i-grid-icon>
+        <i-grid-label>饮品</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/5.png" />
+        </i-grid-icon>
+        <i-grid-label>零食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/6.png" />
+        </i-grid-icon>
+        <i-grid-label>面食</i-grid-label>
+    </i-grid-item>
+</i-grid>
+    <i-panel title="吃货推荐">
+      <view class="top-padding">
+      <i-card title="良品铺子" extra="零食" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">好多不错的零食</view>
+        <view slot="footer">后街1-22</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+    </view>
+    </i-panel>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+.top-padding {
+  padding-top: 50rpx;
+}
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <view class="userinfo">
+    <view class="userinfo-avatar">
+    <open-data type="userAvatarUrl"></open-data>
+    </view>
+    <open-data type="userNickName"></open-data>
+    </view>
+    <i-cell-group>
+    <i-cell title="我的收藏" is-link url="/pages/logs/main"></i-cell>
+    <i-cell title="接收通知">
+        <switch slot="footer" checked />
+    </i-cell>
+</i-cell-group>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+position: relative;
+width: 750rpx;
+height: 320rpx;
+color: #666;
+display: flex;
+flex-direction: column;
+align-items: center;
+}
+ 
+.userinfo-avatar {
+overflow:hidden;
+display: block;
+width: 160rpx;
+height: 160rpx;
+margin: 20rpx;
+margin-top: 50rpx;
+border-radius: 50%;
+border: 2px solid #fff;
+box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+</style>
